14cca5bf48fc89468a046c41a2bbda03b47c282d
Kenneth Wieschhoff <kwieschhoff@mac.com>
2020-12-24 09:13:58 -0800

    Better spacing

--- RedditReader/RedditReader/RedditTableViewCell.swift
+++ RedditReader/RedditReader/RedditTableViewCell.swift
@@ -17,38 +17,71 @@ class RedditTableViewCell: UITableViewCell {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 
         titleLabel.numberOfLines = 0
+        titleLabel.preferredMaxLayoutWidth = UIScreen.main.bounds.width
         titleLabel.lineBreakMode = .byTruncatingTail
         titleLabel.baselineAdjustment = .alignBaselines
         titleLabel.adjustsFontSizeToFitWidth = false
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.frame = CGRect(x: 11, y: 12,width: 394, height:41)
+        titleLabel.frame = CGRect(x: 0, y: 0,width: frame.width, height:21)
+//        titleLabel.layer.borderWidth = 1
+//        titleLabel.layer.borderColor = UIColor.red.cgColor
         contentView.addSubview(titleLabel)
         
         theImage.translatesAutoresizingMaskIntoConstraints = false
         theImage.clipsToBounds = true
         theImage.contentMode = .scaleAspectFit
-        theImage.frame = CGRect(x: 11, y: 56,width: 188, height:153)
+        theImage.frame = CGRect(x: 0, y: 0,width: frame.width, height:153)
+//        theImage.layer.borderWidth = 1
+//        theImage.layer.borderColor = UIColor.red.cgColor
         contentView.addSubview(theImage)
 
         numComments.lineBreakMode = .byTruncatingTail
         numComments.baselineAdjustment = .alignBaselines
         numComments.adjustsFontSizeToFitWidth = false
         numComments.translatesAutoresizingMaskIntoConstraints = false
-        numComments.frame = CGRect(x: 20, y: 217,width: 385, height:21)
+        numComments.frame = CGRect(x: 0, y: 0,width: frame.width, height:21)
+//        numComments.layer.borderWidth = 1
+//        numComments.layer.borderColor = UIColor.red.cgColor
         contentView.addSubview(numComments)
 
         score.lineBreakMode = .byTruncatingTail
         score.baselineAdjustment = .alignBaselines
         score.adjustsFontSizeToFitWidth = false
         score.translatesAutoresizingMaskIntoConstraints = false
-        score.frame = CGRect(x: 20, y: 242,width: 385, height:21)
+        score.frame = CGRect(x: 0, y: 0,width: frame.width, height:21)
+//        score.layer.borderWidth = 1
+//        score.layer.borderColor = UIColor.red.cgColor
         contentView.addSubview(score)
         
-//        let stack = UIStackView(arrangedSubviews: [titleLabel, theImage, numComments,score])
-//        contentView.addSubview(stack)
-     }
+        let views: [String: Any] = [
+            "titleLabel": titleLabel,
+            "theImage": theImage,
+            "numComments": numComments,
+            "score": score]
 
-     required init?(coder aDecoder: NSCoder) {
+        var allConstraints: [NSLayoutConstraint] = []
+        allConstraints += NSLayoutConstraint.constraints(
+          withVisualFormat: "V:|-5-[titleLabel]",
+          metrics: nil,
+          views: views)
+        allConstraints += NSLayoutConstraint.constraints(
+          withVisualFormat: "V:[titleLabel]-10-[theImage]",
+          metrics: nil,
+          views: views)
+        allConstraints += NSLayoutConstraint.constraints(
+          withVisualFormat: "V:[theImage]-10-[numComments]",
+          metrics: nil,
+          views: views)
+        allConstraints += NSLayoutConstraint.constraints(
+          withVisualFormat: "V:[numComments]-10-[score]",
+          metrics: nil,
+          views: views)
+
+        NSLayoutConstraint.activate(allConstraints)
+        
+    }
+
+    required init?(coder aDecoder: NSCoder) {
          fatalError("init(coder:) has not been implemented")
-     }
+    }
 }
--- RedditReader/RedditReader/RedditTableViewController.swift
+++ RedditReader/RedditReader/RedditTableViewController.swift
@@ -55,7 +55,27 @@ class RedditTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 300
+        return cellHeight(index: indexPath.row)
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return cellHeight(index: indexPath.row)
+    }
+    
+    private func cellHeight(index: Int) -> CGFloat {
+        let child = entries[index].data
+        let cell:RedditTableViewCell? = tableView.dequeueReusableCell( withIdentifier: "Cell" )
+            as? RedditTableViewCell
+        if let cell = cell {
+            var height = cell.numComments.frame.height + cell.score.frame.height + cell.theImage.frame.height
+            let constraintRect = CGSize(width: self.view.frame.width, height: .greatestFiniteMagnitude)
+            let boundingBox = child?.title!.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font: cell.titleLabel.font ?? UIFont.systemFont(ofSize: 17)], context: nil)
+
+            height += boundingBox!.height
+            return height + 10
+        }
+
+        return 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -73,6 +93,9 @@ class RedditTableViewController: UITableViewController {
         } else {
             cell?.theImage.image = UIImage(imageLiteralResourceName: "emptyPicture")
         }
+        
+        cell?.layoutSubviews()
+        cell?.layoutIfNeeded()
 
         if cell == nil {
             let newCell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "NewCell" )
